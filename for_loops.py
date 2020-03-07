cf0ea48fed95739bb64c70ee8a5000bd2b3bc0bd
thraway7 <61899054+thraway7@users.noreply.github.com>
2020-03-07 01:26:38 -0500

    Add files via upload

---
+++ for_loops.py
@@ -0,0 +1,86 @@
+
+'''
+for i in [1,2,3,4,5,'a','b','c']:
+    print(i,type(i))
+
+for c in 'orange':
+    print(c)
+
+
+#The variable i becomes each value of the list
+
+'''
+
+#################################
+
+'''
+print(range(10))
+
+for i in range(0,10):
+    print(i)
+
+for i in range(10,20):
+    print(i)
+
+
+for i in range(10,20,2):
+    print (i)
+
+for i in range(100,0,-5):
+    print (i)
+'''
+
+
+''''
+
+a = 'mystring'
+
+len(a)
+
+for i in range(len(a)):
+    print('The characted at position ' + str(i) + ' is ' + a[i])
+
+'''
+
+a = 'mystring'
+
+for(i,j) in enumerate(a):
+    print('The character at position ' + str(i) + ' is ' + j)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
